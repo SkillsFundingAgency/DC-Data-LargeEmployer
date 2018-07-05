db61dbc930d360586cd654b64469593ad7c485e8
Peter Chamley <13586812+PJChamley@users.noreply.github.com>
2018-07-05 16:26:30 +0100

    Fix Typo in Deployment Details stored in Extended Properties of Database

--- src/ESFA.DC.Data.LargeEmployer.Database/zzz.Script.PostDeployment.sql
+++ src/ESFA.DC.Data.LargeEmployer.Database/zzz.Script.PostDeployment.sql
@@ -44,7 +44,7 @@ RAISERROR('		         %s - %s',10,1,'ReleaseName','$(RELEASE_RELEASENAME)') WITH
 IF NOT EXISTS (SELECT name, value FROM fn_listextendedproperty('ReleaseName', default, default, default, default, default, default))
 	EXEC sp_addextendedproperty @name = N'ReleaseName', @value = '$(RELEASE_RELEASENAME)';  
 ELSE
-	EXEC sp_updateextendedproperty @name = N'ReleaseName', @value = '$(BUILD_BRANCHNAME)';  
+	EXEC sp_updateextendedproperty @name = N'ReleaseName', @value = '$(RELEASE_RELEASENAME)';  
 GO
 IF EXISTS (SELECT * FROM [sys].[objects] WHERE [type] = 'V' AND Name = 'DisplayDeploymentProperties_VW')
 BEGIN 
